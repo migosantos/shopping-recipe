649a24c772fac8691e7a7653caf771e2d5a4fea4
Juan Miguel Santos <juanmiguel.santos@silverlakeaxis.com>
2018-06-19 16:49:28 +0800

    header links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  currentScreenName = 'RECIPES';
   title = 'app';
+  onNavigationClicked(screenName: string) {
+    this.currentScreenName = screenName;
+  }
 }
